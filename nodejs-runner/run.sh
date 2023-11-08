2da4061902d20942c7fa064d8d0acb400bbba745
Serge Harb <me@sergeharb.com>
2023-11-08 16:26:42 +0100

    support bun

--- nodejs-runner/run.sh
+++ nodejs-runner/run.sh
@@ -1,25 +1,41 @@
 #!/usr/bin/with-contenv bashio
 
-# Read the repository, branch, and port from the options
+# Read the repository, branch, port, and bun from the options
 PRERUN_SCRIPT_NAME=$(bashio::config 'prerun_script_name')
 SCRIPT_NAME=$(bashio::config 'script_name')
 REPOSITORY=$(bashio::config 'repository')
 BRANCH=$(bashio::config 'branch')
 PORT=$(bashio::config 'port')
+BUN=$(bashio::config 'bun')
 
-# Clone the repository
+echo "1: Cloning the repository"
 git clone --branch "$BRANCH" "$REPOSITORY" /app
 
-# Navigate to the app directory
+echo "2: Navigating to the app directory"
 cd /app
 
-# Install dependencies
-npm install
-
-# Run the pre-run script if exists
-if [ -f "$PRERUN_SCRIPT_NAME" ]; then
-    npm run $PRERUN_SCRIPT_NAME
+echo "3: Installing dependencies"
+if [ "$BUN" = true ]; then
+    echo "Using bun..."
+    bun install
+else
+    echo "Using npm..."
+    npm install
 fi
 
-# Start the application using NPM
-npm run $SCRIPT_NAME -- --port "$PORT"
+echo "4: Running the pre-run script if it exists"
+if [ -f "$PRERUN_SCRIPT_NAME" ]; then
+    echo "Executing pre-run script: $PRERUN_SCRIPT_NAME"
+    npm run $PRERUN_SCRIPT_NAME
+else
+    echo "No pre-run script found."
+fi
+
+echo "5: Starting the application"
+if [ "$BUN" = true ]; then
+    echo "Using bun..."
+    bun run $SCRIPT_NAME --port "$PORT"
+else
+    echo "Using npm..."
+    npm run $SCRIPT_NAME -- --port "$PORT"
+fi
