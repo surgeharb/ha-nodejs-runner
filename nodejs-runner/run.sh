632da5589060761ba3f279e29536b0ce46286f7f
Serge Harb <me@sergeharb.com>
2023-11-08 17:48:52 +0100

    fix port exposing

--- nodejs-runner/run.sh
+++ nodejs-runner/run.sh
@@ -5,8 +5,8 @@ PRERUN_SCRIPT_NAME=$(bashio::config 'prerun_script_name')
 SCRIPT_NAME=$(bashio::config 'script_name')
 REPOSITORY=$(bashio::config 'repository')
 BRANCH=$(bashio::config 'branch')
-PORT=$(bashio::config 'port')
-BUN=$(bashio::config 'bun')
+USE_BUN=$(bashio::config 'use_bun')
+PORT=3250
 
 echo "1: Cloning the repository"
 git clone --branch "$BRANCH" "$REPOSITORY" /app
@@ -15,7 +15,7 @@ echo "2: Navigating to the app directory"
 cd /app || exit
 
 echo "3: Installing dependencies"
-if [ "$BUN" = true ]; then
+if [ "$USE_BUN" = true ]; then
     echo "Installing dependencies using bun..."
     npx bun install
 else
@@ -25,7 +25,7 @@ fi
 
 echo "4: Running the pre-run script if it exists ($PRERUN_SCRIPT_NAME)"
 if [ -n "$PRERUN_SCRIPT_NAME" ]; then
-    if [ "$BUN" = true ]; then
+    if [ "$USE_BUN" = true ]; then
         echo "Running pre-run script using bun..."
         npx bun run "$PRERUN_SCRIPT_NAME"
     else
@@ -37,7 +37,7 @@ else
 fi
 
 echo "5: Starting the application"
-if [ "$BUN" = true ]; then
+if [ "$USE_BUN" = true ]; then
     echo "Using bun..."
     npx bun run "$SCRIPT_NAME" --port "$PORT"
 else
