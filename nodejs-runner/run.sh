dc2be1fd482eecfb92cad681d5e8c15a9b4fb219
Serge Harb <me@sergeharb.com>
2023-11-08 16:03:35 +0100

    add prerun script

--- nodejs-runner/run.sh
+++ nodejs-runner/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
 # Read the repository, branch, and port from the options
+PRERUN_SCRIPT_NAME=$(bashio::config 'prerun_script_name')
 SCRIPT_NAME=$(bashio::config 'script_name')
 REPOSITORY=$(bashio::config 'repository')
 BRANCH=$(bashio::config 'branch')
@@ -14,6 +15,8 @@ cd /app
 
 # Install dependencies
 npm install
+
+npm run $PRERUN_SCRIPT_NAME
 
 # Start the application using NPM
 npm run $SCRIPT_NAME -- --port "$PORT"
